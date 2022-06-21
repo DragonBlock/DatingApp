2c3c05c4c249eee46db720549f1b7ebc259b4c9f
Michael Hoekstra <mhoekstra@mifp.com>
2022-06-21 11:04:16 -0400

    End of Section 17

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -25,6 +25,7 @@ namespace API.Helpers
                     src.Sender.Photos.FirstOrDefault(x => x.IsMain).URL))
                 .ForMember(dest => dest.RecipientPhotoURL, opt => opt.MapFrom(src => 
                     src.Recipient.Photos.FirstOrDefault(x => x.IsMain).URL));
+            CreateMap<DateTime, DateTime>().ConvertUsing(d => DateTime.SpecifyKind(d, DateTimeKind.Utc));
         }
     }
 }
